5b71ea100f976494a57f93a1eb497ceff987215b
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-24 11:23:35 +0530

    Update ssl_chk.sh
    removed 
    $(((expiry_unix-current_unix)/86400)))
    line and added static value

--- scripts/ssl_chk.sh
+++ scripts/ssl_chk.sh
@@ -7,7 +7,7 @@ for i in "$DOMAINS[@]"; do
   days_to_expire=$(echo | openssl s_client -servername "$i" -connect "$i":443 2> /dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/ {print $2}' )
     expiry_unix=(date -d "$days_to_expire" '+%s' )
     current_unix=$(date'+%s')
-    no_of_days_left=$(((expiry_unix-current_unix)/86400)))
+    no_of_days_left=5
     if[[$no_of_days_left -ge 0 && no_of_days_left -le 30]]: then 
     msg="SSL Expiry Alert \n*Domain: $i\n*Domain:$i\n*Warning: The SSL certificate for $i will expire in $no_of_days_left."
     payload="{\"text\":\"$msg\"}"
