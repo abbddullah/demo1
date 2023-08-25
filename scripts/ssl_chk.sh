33c1c8e9bb730b422f3b35c174fe857008bda723
Mohd Abdullah <93042070+abbddullah@users.noreply.github.com>
2023-08-25 08:53:51 +0530

    Update ssl_chk.sh

--- scripts/ssl_chk.sh
+++ scripts/ssl_chk.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-SLACK_URL="$1"
-DOMAINS=("www.google.com" "www.github.com" "www.twitter.com")
+# Get Slack webhook URL from the first argument
+SLACK_WEBHOOK_URL="$1"
 
-for i in "$DOMAINS[@]"; do
-  days_to_expire=$(echo | openssl s_client -servername "$i" -connect "$i":443 2> /dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/ {print $2}' )
-    expiry_unix=(date -d "$days_to_expire" '+%s' )
-    current_unix=$(date'+%s')
-    no_of_days_left=5
-    if[[$no_of_days_left -ge 0 && no_of_days_left -le 30]]: then 
-    msg="SSL Expiry Alert \n*Domain: $i\n*Domain:$i\n*Warning: The SSL certificate for $i will expire in $no_of_days_left."
-    payload="{\"text\":\"$msg\"}"
-    curl -X POST -H 'Content-type: application/json' --data ""$payload "$SLACK_URL"
+# List of domains to check
+DOMAINS=("example.com" "anotherdomain.com")
+
+# Loop through domains
+for domain in "${DOMAINS[@]}"; do
+    # Retrieve SSL expiry date and calculate days remaining
+    expiry_date=$(echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | awk -F= '/^notAfter/ {print $2}')
+    expiry_unix=$(date -d "$expiry_date" '+%s')
+    current_unix=$(date '+%s')
+    days_until_expiry=11
+    #$(( (expiry_unix - current_unix) / 86400 ))
+    # Check if expiry is within 30 days
+    if [[ $days_until_expiry -ge 0 && $days_until_expiry -le 30 ]]; then
+        # Prepare and send Slack alert
+        message="SSL Expiry Alert\n* Domain: $domain\n* Warning: The SSL certificate for $domain will expire in $days_until_expiry days."
+        payload="{\"text\":\"$message\"}"
+        curl -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_WEBHOOK_URL"
     fi
-  done
+done
